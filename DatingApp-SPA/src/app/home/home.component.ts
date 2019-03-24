e710f220d2b7f42fb1690524a8755ab8fa2b3e5e
Rob Simpson <rsimpson@ventera.com>
2019-03-23 23:19:14 -0400

    end of module 4

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -7,27 +7,16 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  values: any;
   registerMode = false;
 
   constructor(
     private http: HttpClient
   ) { }
 
-  ngOnInit() {
-    this.getValues();
-  }
+  ngOnInit() {}
 
   registerToggle() {
     this.registerMode = true;
-  }
-
-  getValues() {
-    this.http.get('http://localhost:5000/api/values')
-      .subscribe({
-        next: (response) => this.values = response,
-        error: (e) => console.log(e)
-      });
   }
 
   onCancel(registerMode: boolean) {
