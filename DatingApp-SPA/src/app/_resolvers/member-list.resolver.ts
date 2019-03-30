cf3077648d893234d1ffca4f0c18e98c2ae2dbe4
Rob Simpson <rsimpson@ventera.com>
2019-03-29 23:02:26 -0400

    member detail view and resolvers

---
+++ DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { User } from '../_models/user';
+import { UserService } from '../services/user.service';
+import { AlertifyService } from '../services/alertify.service';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class MemberListResolver implements Resolve<User[]> {
+  constructor(
+    private userService: UserService,
+    private alertify: AlertifyService,
+    private route: Router
+  ) { }
+
+  resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
+    return this.userService
+      .getUsers()
+      .pipe(
+        catchError(error => {
+          this.alertify.error('Problem loading users');
+          this.route.navigate(['/home']);
+          return of(null);
+        })
+      )
+  }
+}
--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace DatingApp.API.Controllers
 {
     [Authorize]
-    [Route("api/[controller")]
+    [Route("api/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
