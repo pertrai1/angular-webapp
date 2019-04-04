715d6f1dcee4f4be997327bd148cc60617614916
Rob Simpson <rsimpson@ventera.com>
2019-04-03 20:02:08 -0400

    behavior subject for changing photo

--- DatingApp-SPA/src/app/services/auth.service.ts
+++ DatingApp-SPA/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { JwtHelperService } from '@auth0/angular-jwt';
@@ -14,10 +15,16 @@ export class AuthService {
   jwtHelper = new JwtHelperService();
   decodedToken: any;
   currentUser: User;
+  photoUrl = new BehaviorSubject<string>('../../assets/user.png');
+  currentPhotoUrl = this.photoUrl.asObservable();
 
   constructor(
     private http: HttpClient
   ) { }
+
+  changeMemberPhoto(photoUrl: string) {
+    this.photoUrl.next(photoUrl);
+  }
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'login', model)
@@ -29,7 +36,7 @@ export class AuthService {
             localStorage.setItem('user', JSON.stringify(user.user));
             this.decodedToken = this.jwtHelper.decodeToken(user.token);
             this.currentUser = user.user;
-            console.log(this.decodedToken);
+            this.changeMemberPhoto(this.currentUser.photoUrl);
           }
         })
       )
