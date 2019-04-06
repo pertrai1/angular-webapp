c0c940dd83ef03625a320ee0a5177c80dc9bdf28
Rob Simpson <rsimpson@ventera.com>
2019-04-06 15:47:00 -0400

    filtering client side

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -14,6 +14,18 @@ import { AlertifyService } from 'src/app/services/alertify.service';
 })
 export class MemberListComponent implements OnInit {
   users: User[]
+  user: User = JSON.parse(localStorage.getItem('user'));
+  genderList = [
+    {
+      value: 'male',
+      display: 'Males'
+    },
+    {
+      value: 'female',
+      display: 'Females'
+    }
+  ];
+  userParams: any = {};
   pagination: Pagination;
 
   constructor(
@@ -27,6 +39,10 @@ export class MemberListComponent implements OnInit {
       this.users = data['users'].result;
       this.pagination = data['users'].pagination;
     });
+
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
   }
 
   pageChanged(event: any): void {
@@ -34,9 +50,16 @@ export class MemberListComponent implements OnInit {
     this.loadUsers();
   }
 
+  resetFilters() {
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.loadUsers();
+  }
+
   loadUsers() {
     this.userService
-      .getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+      .getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, this.userParams)
       .subscribe(
         (res: PaginatedResult<User[]>) => {
           this.users = res.result;
